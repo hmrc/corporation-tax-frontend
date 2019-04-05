6496f2d272531eb0efcfdb7a878158767f5a7b24
Andrew Shanks <18327122+AndrewShanks@users.noreply.github.com>
2019-04-05 17:43:22 +0100

    BTA-1044: Implemented PaymentRecord object

---
+++ test/utils/CurrencyFormatterSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import base.SpecBase
+
+class CurrencyFormatterSpec extends SpecBase{
+  "The currency formatter" should {
+    "Express a positive sum of money with pounds and pence" in {
+      CurrencyFormatter.formatCurrency(1.23,"£") mustBe "£1.23"
+    }
+
+    "Express a positive sum of money that is a multiple of 10 cents" in {
+      CurrencyFormatter.formatCurrency(1.2, "$") mustBe "$1.20"
+    }
+
+    "Express a positive sum of money that is a whole number of pounds" in {
+      CurrencyFormatter.formatCurrency(1) mustBe "£1"
+    }
+
+    "Express a negative sum of money with pounds and pence" in {
+      CurrencyFormatter.formatCurrency(-1.23) mustBe "-£1.23"
+    }
+
+    "Express a negative sum of money that is a multiple of 10 pence" in {
+      CurrencyFormatter.formatCurrency(-1.2) mustBe "-£1.20"
+    }
+
+    "Express a negative sum of money that is a whole number of pounds" in {
+      CurrencyFormatter.formatCurrency(-1) mustBe "-£1"
+    }
+  }
+
+}
