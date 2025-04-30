e0f8c80f74d9f3558f346df8ecd7d1c35aa8e3cb
@diksha-diksha <176032121+diksha-diksha@users.noreply.github.com>
2025-04-30 14:08:36 +0100

    Updated versions and spec file

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,13 +5,13 @@ private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport._
 
-  val bootstrapVersion = "8.6.0"
+  val bootstrapVersion = "9.11.0"
 
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc" %% "http-caching-client-play-30" % "12.1.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.5.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "11.13.0"
   )
 
   trait TestDependencies {
@@ -40,7 +40,7 @@ private object AppDependencies {
       override lazy val test = Seq(
         "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1" % scope,
         "org.playframework" %% "play-test" % PlayVersion.current % scope,
-        "org.jsoup" % "jsoup" % "1.17.2" % scope,
+        "org.jsoup" % "jsoup" % "1.18.3" % scope,
         "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion  % scope,
         "org.scalatestplus" %% "mockito-5-10" % "3.2.18.0" % scope
       )
