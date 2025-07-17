e901698d6ac9dc43c956a2e95be6b50d6a569255
kannanekambaram <112036677+kannanEkambaram@users.noreply.github.com>
2025-07-17 18:37:06 +0100

    DL-16941 : bobby vulnerability fixes

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ This repository contains the Corporation Tax section of the Business Tax Account
 
  [ ![Download](https://api.bintray.com/packages/hmrc/releases/corporation-tax-frontend/images/download.svg) ](https://bintray.com/hmrc/releases/corporation-tax-frontend/_latestVersion)
 
+## Testing
+Run the below command
+$ sbt clean compile coverage test it:test coverageReport
+Or,
+./run-all-tests.sh
+
 ## License
 
 This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,17 +1,17 @@
-import sbt._
+import sbt.*
 
 private object AppDependencies {
 
   import play.core.PlayVersion
-  import play.sbt.PlayImport._
+  import play.sbt.PlayImport.*
 
-  val bootstrapVersion = "9.13.0"
+  val bootstrapVersion: String = "9.16.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "http-caching-client-play-30" % "12.2.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "11.13.0"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30"   % bootstrapVersion,
+    "uk.gov.hmrc" %% "http-caching-client-play-30"  % "12.2.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"   % "11.13.0"
   )
 
   trait TestDependencies {
@@ -21,7 +21,7 @@ private object AppDependencies {
 
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
-      override lazy val test = Seq("org.mockito" % "mockito-core" % "5.11.0" % scope)
+      override lazy val test: Seq[ModuleID] = Seq("org.mockito" % "mockito-core" % "5.14.2" % scope)
     }.test
   }
 
@@ -29,20 +29,20 @@ private object AppDependencies {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val scope: String = "it"
       override lazy val test: Seq[ModuleID] = Seq(
-        "com.github.tomakehurst" % "wiremock" % "2.33.2" % scope,
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.16.1" % scope)
+        "com.github.tomakehurst"        % "wiremock"              % "2.33.2"  % scope,
+        "com.fasterxml.jackson.module" %% "jackson-module-scala"  % "2.18.0"  % scope)
     }.test
   }
 
   object TestCommon {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val scope: String = "test,it"
-      override lazy val test = Seq(
-        "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1" % scope,
-        "org.playframework" %% "play-test" % PlayVersion.current % scope,
-        "org.jsoup" % "jsoup" % "1.18.3" % scope,
-        "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion  % scope,
-        "org.scalatestplus" %% "mockito-5-10" % "3.2.18.0" % scope
+      override lazy val test: Seq[ModuleID] = Seq(
+        "org.scalatestplus.play"  %% "scalatestplus-play"     % "7.0.1"             % scope,
+        "org.playframework"       %% "play-test"              % PlayVersion.current % scope,
+        "org.jsoup"               % "jsoup"                   % "1.18.3"            % scope,
+        "uk.gov.hmrc"             %% "bootstrap-test-play-30" % bootstrapVersion    % scope,
+        "org.scalatestplus"       %% "mockito-5-10"           % "3.2.18.0"          % scope
       )
     }.test
   }
